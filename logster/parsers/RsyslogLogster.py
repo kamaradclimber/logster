4a858ec05d66d67d6c636ce72fc89795c98a3778
Gregoire Seux <g.seux@criteo.com>
2013-09-03 09:59:03 +0200

    Fix name

--- logster/parsers/RsyslogLogster.py
+++ logster/parsers/RsyslogLogster.py
@@ -11,7 +11,7 @@ import re
 from logster.logster_helper import MetricObject, LogsterParser
 from logster.logster_helper import LogsterParsingException
 
-class RsyslogLogLogster(LogsterParser):
+class RsyslogLogster(LogsterParser):
 
     def __init__(self, option_string=None):
         '''Initialize any data structures or variables needed for keeping track

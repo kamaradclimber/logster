489e4444f9dfd835961cedd56e3921dd03ea4a80
Yann Schwartz <y.schwartz@criteo.com>
2014-08-14 14:39:29 +0200

    Simplify regex and add white list
    * Better metric name detection
    * Simpler sanitization
    * Make it possible to supply list of metrics to keep

--- logster/parsers/RsyslogLogster.py
+++ logster/parsers/RsyslogLogster.py
@@ -7,6 +7,7 @@
 
 import time
 import re
+import optparse
 
 from logster.logster_helper import MetricObject, LogsterParser
 from logster.logster_helper import LogsterParsingException
@@ -16,31 +17,41 @@ class RsyslogLogster(LogsterParser):
     def __init__(self, option_string=None):
         '''Initialize any data structures or variables needed for keeping track
         of the tasty bits we find in the log we are parsing.'''
-        self.values = dict()
+        self.whitelist = None
+        if not option_string is None:
+            parser = optparse.OptionParser()
+            options = option_string.split(' ')
+            parser.add_option("--whitelist", "-w", dest='whitelist', default='',
+                    help='comma separated list of actions/queues to keep')
+            opts, args = parser.parse_args(options)
+            self.whitelist = set(opts.whitelist.split(','))
 
+        self.values = dict()
         # Regular expression for matching lines we are interested in, and capturing
         # fields from the line
         #self.reg = re.compile('.*rsyslogd-pstats: main Q: size=(\d+) enqueued=(\d+) full=(\d+) discarded.full=(\d+) discarded.nf=(\d+) maxqsize=(\d+)')
-        self.reg_check = re.compile('.*rsyslogd-pstats: ([\w\s]+):')
+        self.reg_check = re.compile('.*rsyslogd-pstats:\s+([^:]+):')
         self.reg = re.compile('([\w\.]+)=(\d+)')
+        self.reg_clean = re.compile(r'[^A-Za-z\d-]')
 
 
     def parse_line(self, line):
         '''This function should digest the contents of one line at a time, updating
         object's state variables. Takes a single argument, the line to be parsed.'''
-
         try:
             # Apply regular expression to each line and extract interesting bits.
             rsyslog = self.reg_check.match(line)
             regMatch = self.reg.findall(line)
 
             if rsyslog and regMatch:
-                queue = re.sub(r"[\s\.]","", rsyslog.groups()[0]).lower()
+                queue = self.reg_clean.sub("", rsyslog.groups()[0]).lower()
+                if not self.whitelist is None and not (queue in self.whitelist):
+                    return
                 for name,value in regMatch:
-                    metric = queue + '.' + re.sub(r'\.', '', name)
+                    metric = queue + '.' + name.replace('.','')
                     self.values[metric] = value
             else:
-                raise "No match"
+                raise Exception("No match")
 
         except Exception, e:
             raise LogsterParsingException, "regmatch or contents failed with %s" % e
---
+++ tests/tests_rsyslogparser.py
@@ -0,0 +1,21 @@
+from logster.parsers.RsyslogLogster import RsyslogLogster
+import unittest
+
+
+class TestRsyslogParser(unittest.TestCase):
+
+    def test_whitelist_should_filter(self):
+        whitelist = "--whitelist foo,bar"
+        parser = RsyslogLogster(option_string=whitelist)
+        lines = [
+            "2014-08-14T09:10:42.716176+00:00 fc rsyslogd-pstats: foo: processed=0 failed=0",
+            "2014-08-14T09:10:42.716177+00:00 fc rsyslogd-pstats: imudp(*:514): submitted=0",
+            "2014-08-14T09:10:42.716178+00:00 fc rsyslogd-pstats: bar: submitted=0"
+        ]
+        for line in lines:
+            parser.parse_line(line)
+
+        names = set([k.split('.')[0] for k in parser.values.keys()])
+        self.assertEquals(2, len(names))
+        self.assertTrue('foo' in names)
+        self.assertTrue('bar' in names)
